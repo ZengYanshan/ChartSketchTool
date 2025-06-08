5445234b2ae88f8724a9bec2fe1d072f00af0978
ZengYanshan <1309161028@qq.com>
2025-06-08 15:38:09 +0800

    增加对数据洞察类型的中文翻译

--- www/src/js/insight-type.js
+++ www/src/js/insight-type.js
@@ -11,3 +11,16 @@ const insightTypes = [
     "skewness",
     "kurtosis",
 ];
+
+const insightTypeTranslation = {
+    "dominance": "主导值",
+    "top2": "双主导值",
+    "outlier": "离群值",
+    "outlier-temporal": "时序离群值",
+    "trend": "趋势",
+    "correlation": "相关性",
+    "correlation-temporal": "时序相关性",
+    "evenness": "均匀度",
+    "skewness": "偏度",
+    "kurtosis": "峰度",
+}
